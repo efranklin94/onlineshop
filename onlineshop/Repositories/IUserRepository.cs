b87787e4ec36a91ff73344cf5640702a147e93f0
Eddy Franklin <eefathi@live.com>
2025-04-26 15:16:37 +0330

    add: repository layer

---
+++ onlineshop/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using onlineshop.Models;
+
+namespace onlineshop.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task AddAsync(MyUser modelA, CancellationToken cancellationToken);
+        public void Update(MyUser modelA);
+        public void Delete(MyUser modelA);
+        public Task<MyUser?> GetByIdAsync(int id, CancellationToken cancellationToken);
+        public Task<List<MyUser>> GetListAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ onlineshop/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using onlineshop.Models;
+
+namespace onlineshop.Repositories
+{
+    public class UserRepository(MyDbContext db) : IUserRepository
+    {
+        private readonly DbSet<MyUser> set = db.Set<MyUser>();
+
+        public async Task AddAsync(MyUser modelA, CancellationToken cancellationToken)
+        {
+            await set.AddAsync(modelA, cancellationToken);
+        }
+
+        public void Update(MyUser modelA)
+        {
+            set.Update(modelA);
+        }
+
+        public void Delete(MyUser modelA)
+        {
+            set.Remove(modelA);
+        }
+
+        public async Task<MyUser?> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            return await set.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public async Task<List<MyUser>> GetListAsync(CancellationToken cancellationToken)
+        {
+            return await set.AsNoTracking().ToListAsync(cancellationToken);
+        }
+    }
+}
