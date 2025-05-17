128b50569f9409ad90d51c7a3e5a88cc3fe4c1b4
Eddy Franklin <eefathi@live.com>
2025-05-17 14:25:47 +0330

    add: trackingcode app

---
+++ TrackingCode/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/TrackingCode/{prefix}", ([FromRoute] string prefix) =>
+{
+    return $"{prefix}-{Random.Shared.Next(10000, 99999)}";
+})
+.WithName("TrackingCode");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
